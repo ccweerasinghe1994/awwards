b198c87ba8b15d3cb5dd3c24c17fc56b706a9b61
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-11-24 16:58:01 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <main>
+            <h1 className={"text-5xl text-orange-500 font-bold"}>Well Come to Awards</h1>
+        </main>
+    )
+}
+
+export default App
